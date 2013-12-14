7772f954124612e116fdd27d3eb7b366750ff137
oscar <oscar@atenas.babilonia>
2013-12-13 23:53:30 -0300

    Last

--- crawl.py
+++ crawl.py
@@ -5,12 +5,12 @@ from std_http import std_http
 from std_web import std_web
 
 
-url = "http://www.duoc.cl/englishprogram/"
+url = "http://drupalqa.duoc.cl/"
 
 #http = std_http()
 #web = std_web()
 db = std_db();
-db_args = {'host':'127.0.0.1', 'user':'root', 'passwd':'hello'}
+db_args = {'host':'127.0.0.1', 'user':'root', 'passwd':'websites'}
 db.setParams(db_args)
 db.connect();
 db.selectDb('crawl')
@@ -19,7 +19,7 @@ db.execute("""TRUNCATE TABLE anchor""");
 
 website = websitecrawl();
 website.set_parameter('db', db);
-website.set_parameter('restrict','/englishprogram/')
+#website.set_parameter('restrict','/englishprogram/')
 website.start_crawl(url);
 
 
